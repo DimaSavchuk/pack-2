bccc72f3c2e47ab453c98102271c23b791d670ce
valeri143 <valeria23200523251605@gmail.com>
2023-06-10 16:31:48 +0300

    commit

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -2,8 +2,49 @@ import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
+const LS_KEY = 'todos'
 export class Todos extends Component {
+  state = {
+    todos: [],
+  }
+  componentDidMount(){
+    const savedTodos = localStorage.getItem(LS_KEY);
+   if (savedTodos){
+    this.setState({todos: JSON.parse(savedTodos)})
+   }
+  }
+  componentDidUpdate = (prevProps, prevState) => {
+    const {todos} = this.state;
+    if(prevState.todos !== todos){
+      localStorage.setItem(LS_KEY, JSON.stringify(todos))
+    }
+  }
+  
+  addTodo =(text) =>{
+    const newTodo ={
+      text,
+      id: nanoid()
+    }
+    this.setState(prevState => ({todos: [...prevState.todos, newTodo]}))
+  }
+  deleteTodo = (id) =>{
+this.setState(prevState => ({todos: prevState.todos.filter(item => item.id !== id)}))
+  }
   render() {
-    return <Text>Todos</Text>;
+    const {todos} = this.state;
+    return (
+      <>
+       <SearchForm onSubmit={this.addTodo} />
+       <Grid>{todos.map(({text, id}, idx) => (
+        <GridItem key={id}>
+        <Todo text={text} count={idx + 1} onDelete={this.deleteTodo} id={id}/>
+      </GridItem>
+       )
+       
+       )}</Grid>
+       </>
+      
+    );
+
   }
 }
